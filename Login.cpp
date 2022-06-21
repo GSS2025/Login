eb852a031a3e8d337096adc0c19715f227608e9a
GSS <gurbirsingh2025@gmail.com>
2022-06-21 16:20:31 -0400

    V0.01
    -initial plan created in txt
    -cpp file now set to print basic messages to user

--- Login.cpp
+++ Login.cpp
@@ -2,5 +2,7 @@
 
 int main(){
     using namespace std;
-    
+    cout << "Welcome to the Login and Registration System!";
+    cout << "There is currently no logins or registrartions allowed at this time.";
+    cout << "Please come back later and try again!";
 }
